15bfbdce9fd51f083ab489f183089f28add39a88
HoanghuytranT2207A <110473091+HoanghuytranT2207A@users.noreply.github.com>
2022-08-06 23:21:27 +0700

    Add files via upload

---
+++ Bai tap 3.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+
+int a,b,c;
+printf("nhap a=");
+scanf("%d",&a);
+printf("nhap b=");
+scanf("%d",&b);
+printf("nhap c=");
+scanf("%d",&c);
+
+if(b=1){
+	printf("c=a");
+}else
+    if(b=2){
+	printf("c= a+31");
+}else
+    if(b=3){
+	printf("c=a+59");
+}else
+    if(b=4){
+	printf("c=a+89");
+}else
+    if(b=5){
+    printf ("c= a+120");
+}else
+    if (b=6){
+    printf("c= a+150");
+}else
+    if (b=7){
+    printf("c=a+181");
+}else
+    if (b=8){
+    printf("c=a+211");
+}else
+   if (b=9){
+    printf("c=a+242");
+}else
+    if(b=10){
+	printf("c=a+272");
+}else
+    if (b= 11){
+	printf("c=a+303");
+}else
+    if(b= 12){
+	printf("c=a+333");	
+	}
+}
+
+
+		
+  
