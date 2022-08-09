105073b51d1d16f2c1243a9cfc15ad3708fc611b
HoanghuytranT2207A <110473091+HoanghuytranT2207A@users.noreply.github.com>
2022-08-09 23:09:24 +0700

    Add files via upload

---
+++ Assignment 3 Number 1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+	int n,y;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	printf("nhap y=");
+	scanf("%d",&y);
+	
+	y=3
+	while(y<n){
+	 int r=y%2;
+	 r=1;
+	 y= y++;
+    }  
+	printf("Number y=");
+}
+
---
+++ Assignment 3 Number 2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+	int n,y;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	printf("Nhap y=");
+	scanf("%d",&y);
+	
+	y=6;
+	while(y<n){
+		int r= y%2;
+		int k=y%3;
+        r=0;
+        k=0;
+        y++;
+    } 
+		printf("so y =");
+        
+}
