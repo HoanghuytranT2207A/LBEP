84cb6599301107fc5ab4d1732c87054078683320
HoanghuytranT2207A <110473091+HoanghuytranT2207A@users.noreply.github.com>
2022-08-10 23:23:32 +0700

    Add files via upload

---
+++ Assignment deadline 23-59 august 10th versione 2 use while.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("nhap n=");
+	scanf("%d",&n);
+	
+	int i=1;
+	while(i=<n){
+		n\i=1;
+		i++;
+	}
+	 printf("n la so nguyen to:%d,n");
+}
---
+++ Assignment deadline 23-59 august 10th.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("nhap n=");
+	scanf("%d",&n);
+	
+	for(int i=1;i=<n;i++){
+	n\i=1
+	}
+	printf("n la so nguyen to:%d,n");
+}
+
